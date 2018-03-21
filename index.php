637dfa4990439179a1d18562fc76ec29f8484f78
synne <synne-94@hotmail.com>
2018-03-21 23:11:23 +0100

    Lastet opp dette prosjektet
    Dette ble gjennomført for en stund tilbake.

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Deliverable 3</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="print.css" media="print">
+        <link href="del3.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div  class="mobilNav">
+            <h2>Menu</h2>
+            <nav>
+                <a href="">Home</a>
+                <a href="#del2">How it works</a>
+                <a href="#del3">Description</a>
+            </nav>
+        </div>
+        <div class="topNav">    
+            <nav>
+                <a href=""><img alt="Our logo, it says Care planner with a checkbox" src="http://i.imgur.com/cM1q4DE.png"></a>
+                <a href=""><div>Home</div></a>
+                <a href="#del2"><div>How it works</div></a>
+                <a href="#del3"><div>Description</div></a>
+            </nav>
+        </div>
+        <div id="del1">
+            <div id="text1">
+                <p class="stortekst">Do you know anyone who needs a little extra attendance in their everyday life?</p>
+                <p class="ital stortekst">Are there many people who wants to help, but struggle to organize the tasks? CarePlanner is your solution!</p>
+                <p>
+                    Think of the scenario where your mother or grandmother is getting older and are in need of assistance in her everyday life. She needs help to go get groceries, go to the doctor, and maybe to make dinner. You and your family wants to help. One day, your sister is going to take grandma to the doctor. She calls you after, and asks if it went well at the doctors. She thought you were going to take her, and you thought she would do it. It ends up with grandma missing her appointment.
+                </p>
+                <p class="bold">This could have been avoided with CarePlanner!</p>
+            </div>
+        </div>
+        <div id="del2">
+            <div id="img1" class="bilder" alt="This is the home screen of our app, where you can chose and add persons to care for" ></div> 
+            <div class="imgpil" alt="An arrow pointng to the new image" ></div>
+            <div id="img2" class="bilder" alt="This is the task page where you can see which tasks that you have chosen to take care of"></div> 
+            <div class="imgpil" alt="An second arrow pointng to the new image"></div>
+            <div id="img3" class="bilder" alt="This is the page were you can see who is taking care of wich tasks"></div> 
+        </div>
+        <div id="del3">
+            <div id="text3">
+
+                <p>Get started:</p> 
+                <div id="logoer">
+                    <img class="dllogo" src="https://scontent-arn2-1.xx.fbcdn.net/hphotos-xpf1/v/t35.0-12/12204308_915067921875006_1574468687_o.jpg?oh=db492ac4413e6a86ffaa1bfeca1821a8&oe=563DD30E" alt="A picture of the google play download logo">
+                    <img class="dllogo" src="https://upload.wikimedia.org/wikipedia/commons/5/5d/Available_on_the_App_Store_(black).png" alt="A picture of the app-store download logo">
+                </div>
+                <ul>
+                    <li>Download the app from app store or google play, or enter the website.</li>
+                    <li>Add a profile for the person in need of care.</li>
+                    <li>Add resource people (family and friends). They will get an e-mail with further information about the application (how to log in and how it works).</li>
+                    <li>You and the others can add tasks, recurring or single events.</li>
+                    <li>Share the tasks between you.</li>
+                </ul>
+
+                <p>Everyday use:</p>
+                <ul>
+                    <li>See all your tasks.</li>
+                    <li>Click the checkbox when the task is completed. The checkbox is visible for everyone in the group, that makes it easier for them to keep track of the completed tasks.</li>
+                    <li>If you can’t complete a task, you can send it to one of the others through the application, and ask if they can do it. (They receive the message through their chosen option; SMS, e-mail or push-notification.)</li>
+                </ul>
+                <p>Other functions:</p>
+                <ul>
+                    <li>You can add as many user profiles as you want.</li>
+                    <li>There are no predesigned tasks in the application, so you can define the tasks to fit your needs.</li>
+                    <li>You have the possibility to select time intervals for recurring tasks. For example making dinner is set to repeat once a day, walking the dog three times a day, while vacuuming once a week.</li>
+                    <li>You can add deadlines to important tasks and they will automatically appear on the top of your to-do-list.</li>
+                </ul>
+                <p>Data we will collect:</p>
+                <p class="liten">To collect data, we will be using different sensors. Touchscreen for a smartphone or tablet, and a keyboard/mouse for the desktop version. The users of our application will have to implement information about the person in need of care, as well as information about the caretakers. The caretakers will be the ones using the application. They have to specify the tasks that needs to be performed, and share the tasks between the members of the group. This will be synchronized with all devices associated to this group, so the completed tasks will be visible for everyone.</p>
+                <p class="liten">The data we will be collecting are implemented by the users. Name, phone number, e-mail address and a picture, are personal information required from the user. Only limited information about the person in need of care is required.</p>
+
+            </div>
+            <hr>
+            <ul id="footer">
+                <li>Keren Elene Danielsen, s235899</li>
+                <li>Thea Elise Andersen, s305495</li>
+                <li>Venicia Punithan, s305490</li>
+                <li>Synne Ellefsen, s305463</li>
+                <li>Julian Hjallum, s305501</li>
+            </ul>
+        </div>
+    </body>
+</html>
